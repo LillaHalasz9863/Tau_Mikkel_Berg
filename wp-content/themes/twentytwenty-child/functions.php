e65b17675d4fdd943c913fc4bf9b8b5f5a633665
LillaHalasz9863 <halasz.lilla.lkg@gmail.com>
2020-05-19 16:22:49 +0200

    trying to add js

--- wp-content/themes/twentytwenty-child/functions.php
+++ wp-content/themes/twentytwenty-child/functions.php
@@ -5,5 +5,8 @@
 	function tt_child_enqueue_parent_styles() {
 	   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 	}
-
+	function my_custom_scripts() {
+		wp_enqueue_script( 'custom-js', get_stylesheet_directory_uri() . '/slideshow.js', array( 'jquery' ),'',true );
+	}
+	add_action( 'wp_enqueue_scripts', 'my_custom_scripts' );
 ?>
