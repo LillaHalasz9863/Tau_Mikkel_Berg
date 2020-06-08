8075fbe76903a0aba35a5f18bfb0009d604ac049
LillaHalasz9863 <halasz.lilla.lkg@gmail.com>
2020-06-08 10:43:38 +0200

    last couple changes

--- wp-content/themes/twentytwenty-child/accordion.js
+++ wp-content/themes/twentytwenty-child/accordion.js
@@ -4,11 +4,11 @@ var i;
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
+    var textbox = this.nextElementSibling;
+    if (textbox.style.display === "block") {
+      textbox.style.display = "none";
     } else {
-      panel.style.display = "block";
+      textbox.style.display = "block";
     }
   });
 }
